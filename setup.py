108a7bf58be182249381b6c0c45f55438748c890
ruivapps <rui.li@logmein.com>
2018-02-05 13:24:05 -0500

    to work with PyPi, change README.md into README.rst

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 import setuptools
 
+_version_ = '1.0'
+
 setuptools.setup(
-    name="jenkins2-job-builder",
-    version='1.0',
-    author='Rui Li',
+    name = "jenkins2-job-builder",
+    version = _version_,
+    author = 'Rui Li',
     author_email = 'rui.li.spam@gmail.com',
-    description="temporary solution to push jobs to Jenkins2 due to jenkins-job-builder currently is brokwn on Jenkins2",
-    license="MIT License",
-    url="https://github.com/ruivapps/jenkins2-job-builder",
-    download_url='https://github.com/ruivapps/jenkins2-job-builder/tarball/1.0',
-    packages=["jenkins2_job_builder"],
-    package_dir={'jenkins2_job_builder':'jenkins2_job_builder'},
-    package_data={'jenkins2_job_builder': ['*.py']},
-    install_requires=['requests', 'jenkins-job-builder', 'PyYAML'],
-    classifiers=[
+    description = "temporary solution to push jobs to Jenkins2 due to jenkins-job-builder currently is brokwn on Jenkins2",
+    long_description = open('README.rst').read(),
+    license = "MIT License",
+    url = "https://github.com/ruivapps/jenkins2-job-builder",
+    download_url = 'https://github.com/ruivapps/jenkins2-job-builder/tarball/{}'.format(_version_),
+    packages = ["jenkins2_job_builder"],
+    package_dir = {'jenkins2_job_builder':'jenkins2_job_builder'},
+    package_data = {'jenkins2_job_builder': ['*.py']},
+    install_requires = ['requests', 'jenkins-job-builder', 'PyYAML'],
+    classifiers = [
         "Environment :: Console",
         "Topic :: Utilities",
         "Intended Audience :: Developers",
@@ -27,6 +30,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
     ],
-    entry_points={
+    entry_points = {
         'console_scripts':['jenkins2-jobs=jenkins2_job_builder.jenkins2_jobs:main']},
 )
